f818d0b0ae3069a9a805fe0094dce7bf75c1dc80
Rem <kerem.bulut.006@gmail.com>
2025-11-09 23:01:45 +0100

    Updated to port 8000

--- semiubot.py
+++ semiubot.py
@@ -9,7 +9,7 @@ def home():
     return "Bot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8000)
 
 threading.Thread(target=run).start()
 # --- end keep-alive web server ---
